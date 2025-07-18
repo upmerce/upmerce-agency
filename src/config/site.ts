231c3ed3570263abc85524036a04f7732bb07c31
unknown <upmerce.com@gmail.com>
2025-07-18 12:46:00 +0100

    tourism demo url changed2

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,7 +5,7 @@ export const SITE_NAME = "Upmerce Solutions";
 // Other true constants could go here, for example:
 export const COMPANY_FOUNDER = "Mustapha Ouazza";
 export const SUPPORT_EMAIL = "support@upmercesolutions.com";
-export const TOURISM_DEMO = "tourism-agency-snowy.vercel.app";
+export const TOURISM_DEMO = "https://tourism-agency-snowy.vercel.app";
 
 // You can add more constants as needed for your site configuration
 export const COPYRIGHT_YEAR = new Date().getFullYear();
